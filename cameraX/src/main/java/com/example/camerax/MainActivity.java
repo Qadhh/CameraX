aaca6e7c9b3b1ef901f65e3cdf04374cab387c16
zhangxu <www.895755989@qq.com>
2022-10-14 18:26:19 +0800

    使用 ViewBinding

--- cameraX/src/main/java/com/example/camerax/MainActivity.java
+++ cameraX/src/main/java/com/example/camerax/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.camerax.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private com.example.camerax.databinding.ActivityMainBinding mViewBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mViewBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mViewBinding.getRoot());
     }
 }
