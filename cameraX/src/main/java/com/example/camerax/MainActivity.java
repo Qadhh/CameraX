0f6486357a53a35ec66cda07532c8e1ce35226ad
zhangxu <www.895755989@qq.com>
2022-10-14 19:30:50 +0800

    设置 MainActivity 添加权限

--- cameraX/src/main/java/com/example/camerax/MainActivity.java
+++ cameraX/src/main/java/com/example/camerax/MainActivity.java
@@ -1,19 +1,96 @@
 package com.example.camerax;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.camerax.databinding.ActivityMainBinding;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
     private com.example.camerax.databinding.ActivityMainBinding mViewBinding;
+    private static final int REQUEST_CODE_PERMISSIONS = 10;
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"};
+    private ExecutorService mExecutorService;
+    private ExecutorService mCameraExecutor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mViewBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mViewBinding.getRoot());
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera();
+        } else {
+            ActivityCompat.requestPermissions(
+                    this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        }
+
+        // Set up the listeners for take photo and video capture buttons
+        mViewBinding.imageCaptureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePhoto();
+            }
+        });
+        mViewBinding.videoCaptureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                captureVideo();
+            }
+        });
+        mCameraExecutor = Executors.newSingleThreadExecutor();
     }
+
+    private boolean allPermissionsGranted() {
+        for (String permission : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    private void startCamera() {
+    }
+
+    private void takePhoto() {
+    }
+
+    private void captureVideo() {
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            //已授予权限，请调用 startCamera()
+            if (allPermissionsGranted()) {
+                startCamera();
+            } else {//未授予权限，显示一个消息框，通知用户未授予权限
+                Toast.makeText(this,
+                        "用户未授予权限",
+                        Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        mCameraExecutor.shutdown();
+    }
+
+
 }
